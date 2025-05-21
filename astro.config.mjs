97c377cc021b47020fc8ef14a7f45c3cac921b7a
Sam Ducker <sam@luckyduck.digital>
2025-05-21 12:33:45 +0200

    remove unused cloudflare related code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import cloudflare from "@astrojs/cloudflare";
 import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
 import { storyblok } from "@storyblok/astro";
@@ -88,14 +87,5 @@ export default defineConfig({
   vite: {
     //@ts-ignore
     plugins: [tailwindcss()],
-    resolve: {
-      // Use react-dom/server.edge instead of react-dom/server.browser for React 19.
-      // Without this, MessageChannel from node:worker_threads needs to be polyfilled.
-      alias: import.meta.env.PROD
-        ? {
-            "react-dom/server": "react-dom/server.edge",
-          }
-        : {},
-    },
   },
 });
